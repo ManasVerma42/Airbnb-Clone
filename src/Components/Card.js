52cf4cb7b69f255bcd0f7ca6279b64abfeb31316
ArachnoStarkCodes <manas.sudip.verma9426@gmail.com>
2024-07-20 00:00:19 +0530

    created a card element

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,14 +3,15 @@ import React from "react";
 export default function Card() {
     return (
         <div className="card">
-            <img src="../../img/img1.png" alt="img1"/>
+            <img className="card-image" src="../../img/img1.png" alt="img1"/>
             <div className="card-stats">
-                <img src="../../img/star.png" alt="star"/>
+                <img className="card-star" src="../../img/star.png" alt="star"/>
                 <span>5.0</span>
-                <span></span>
-            
-
+                <span className="gray">(6) </span>
+                <span className="gray">USA</span>
             </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136 /</span>person</p>
         
         </div>
     );
