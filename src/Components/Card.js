5087608387612cd0fff6ab8fe43e61d3c6be053e
ArachnoStarkCodes <manas.sudip.verma9426@gmail.com>
2024-07-19 23:51:02 +0530

    added star

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,15 @@ import React from "react";
 export default function Card() {
     return (
         <div className="card">
+            <img src="../../img/img1.png" alt="img1"/>
+            <div className="card-stats">
+                <img src="../../img/star.png" alt="star"/>
+                <span>5.0</span>
+                <span></span>
             
+
+            </div>
+        
         </div>
     );
 }
