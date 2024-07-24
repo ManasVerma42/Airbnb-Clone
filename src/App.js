d136cb433c06f2becd1f455b4630e3eb7fe815a8
ManasVerma42 <manas.sudip.verma9426@gmail.com>
2024-07-24 23:11:10 +0530

    Added status badge

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ export default function App() {
 	const cards = data.map(item => {
 		return(
 			<Card 
-				img = {item.coverImg}
-				rating = {item.stats.rating}
-				reviewCount = {item.stats.reviewCount}
-				location = {item.location}
-				title = {item.title}
-				price = {item.price}
+				key = {item.key}
+				{...item} // passes all the components as a list of all properties
+				// We could also have passed the item as an object to the prop
+				// img = {item.coverImg}
+				// rating = {item.stats.rating}
+				// reviewCount = {item.stats.reviewCount}
+				// location = {item.location}
+				// title = {item.title}
+				// price = {item.price}
+				// openSpots = {item.openSpots}
 			/>
 		)
 	});
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 export default function Card(props) {
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online"){
+        badgeText = "ONLINE"
+    }
+
+
     return (
         <div className="card">
-            <img className="card-image" src={`./img/${props.img}`} alt="img1"/>
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+            <img className="card-image" src={`./img/${props.coverImg}`} alt="img1"/>
             <div className="card-stats">
                 <img className="card-star" src="./img/star.png" alt="star"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) </span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) </span>
                 <span className="gray">{props.location}</span>
             </div>
             <p className="card-title">{props.title}</p>
